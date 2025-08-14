171a9f555feed661704944cb74386ce403168df0
Felipe Alievi <FELIPEALIEVI1@GMAIL.COM>
2025-08-14 19:41:24 -0300

    Última parte

--- app.js
+++ app.js
@@ -4,10 +4,52 @@ function sortear() {
     let ate = parseInt(document.getElementById('ate').value);
 
     let sorteados = [];
+    let numero;
 
-    for (let i = 0; i < quantidade; i++);
+    if (de >= ate) {
+        alert(`O número minímo ${de} é maior que o número máximo ${ate}! Tente novamente.`)
+        return
+    }
+
+    if (quantidade > ate - de + 1){
+        alert(`A quantidade ${quantidade} de números supera a quantidade total no intervale entre ${de} e ${ate}! Tente novamente.`)
+        return
+    }
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+    alterarStatusBotao();
 }
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    getElementById('resultado').innerHTML = 'Números sorteados: nenhum até agora';
+    alterarStatusBotao();
 }
